f7efa3d304ff3890c85bb0252fd0ac99a66b61c8
Mohamad Ikhsan Nurulloh <32507742+ikhsannur1996@users.noreply.github.com>
2025-10-07 20:18:25 +0700

    Update table_ddl.sql

--- table_ddl.sql
+++ table_ddl.sql
@@ -1,37 +1,27 @@
--- public.factory_production definition
-
--- Drop table
-
--- DROP TABLE public.factory_production;
-
-CREATE TABLE public.factory_production (
-	fact_id serial4 NOT NULL,
-	factory_id int4 NOT NULL,
-	factory_name varchar(100) NULL,
-	"location" varchar(100) NULL,
-	industry_type varchar(100) NULL,
-	factory_size varchar(20) NULL,
+CREATE TABLE public.factory_data (
+	factory_id int4 NULL,
+	factory_name varchar(50) NULL,
+	industry_type varchar(50) NULL,
 	number_of_employees int4 NULL,
-	manager_name varchar(100) NULL,
-	certification varchar(50) NULL,
-	main_products varchar(100) NULL,
+	production_technology_level varchar(50) NULL,
+	quality_certification varchar(50) NULL,
+	supply_chain_integration varchar(50) NULL,
+	lean_manufacturing_practice varchar(50) NULL,
+	maintenance_strategy varchar(50) NULL,
+	six_sigma_maturity_level varchar(50) NULL,
+	automation_level varchar(50) NULL,
+	employee_training_program varchar(50) NULL,
 	"year" int4 NULL,
 	total_units_produced int4 NULL,
 	defective_units int4 NULL,
-	operational_cost_idr int8 NULL,
-	maintenance_cost_idr int8 NULL,
 	total_hours_worked int4 NULL,
 	overtime_hours int4 NULL,
-	energy_consumed_kwh int8 NULL,
-	safety_incidents varchar(20) NULL,
-	employee_performance float8 NULL,
-	defect_rate float8 NULL,
-	cost_per_unit_idr float8 NULL,
-	energy_efficiency_kwh_per_unit float8 NULL,
-	shift_type varchar(20) NULL,
+	energy_consumed_kwh int4 NULL,
+	safety_incidents int4 NULL,
+	employee_performance float4 NULL,
+	defect_rate float4 NULL,
+	energy_efficiency_kwh_per_unit varchar(50) NULL,
 	employee_satisfaction int4 NULL,
-	production_line varchar(20) NULL,
 	machine_downtime_hours int4 NULL,
-	waste_generated_kg int4 NULL,
-	CONSTRAINT factory_production_pkey PRIMARY KEY (fact_id)
+	waste_generated_kg int4 NULL
 );
